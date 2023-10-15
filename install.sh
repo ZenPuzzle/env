9ef5911ea726cad6a0797b38db7c232530ba249e
ZenPuzzle <9865240+ZenPuzzle@users.noreply.github.com>
2023-10-15 21:18:31 +0200

    added incomplete install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin bash
+env_root = `realpath .`
+ln -s "$env_root/dotfiles/vimrc" ~/.vimrc
+ln -s "$env_root/dotfiles/vim" ~/.vim
+ln -s "$env_root/dotfiles/tmux.conf" ~/.tmux.conf
+ln -s "$env_root/dotfiles/tmux" ~/.tmux

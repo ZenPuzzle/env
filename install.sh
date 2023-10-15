aa688617a997d90d4287ebdc2610bb8daa8f3f7d
ZenPuzzle <9865240+ZenPuzzle@users.noreply.github.com>
2023-10-15 21:23:00 +0200

    fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin bash
-env_root = `realpath .`
+env_root=`realpath .`
 ln -s "$env_root/dotfiles/vimrc" ~/.vimrc
 ln -s "$env_root/dotfiles/vim" ~/.vim
 ln -s "$env_root/dotfiles/tmux.conf" ~/.tmux.conf

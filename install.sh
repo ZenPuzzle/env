fee0d16d5df67490c1178d81ca28d715eb23e8d5
ZenPuzzle <9865240+ZenPuzzle@users.noreply.github.com>
2024-10-30 21:45:47 +0100

    nvim init

--- install.sh
+++ install.sh
@@ -2,25 +2,6 @@
 set -eu
 env_root=`realpath "$PWD"`
 
-create_link() {
-  local file=$1
-  local executable=$2
-  if [ -e "$HOME/.$file" ]; then
-    echo "Warning: ~/.$file already exists."
-  elif [ -n "$executable" ] && ! command -v $executable &> /dev/null; then
-    echo "Warning: $executable executable not found. Skipping link for $file."
-  else
-    ln -fs "$env_root/dotfiles/$file" "$HOME/.$file"
-  fi
-}
-
-dotfiles=("tmux.conf" "tmux" "aider.conf.yml" "vim" "vimrc" "hyper.js" "alacritty.yml")
-executables=("" "" "aider" "vim" "vim" "hyper" "alacritty")
-
-for i in "${!dotfiles[@]}"; do
-  create_link "${dotfiles[$i]}" "${executables[$i]}"
-done
-
 if ! command -v nvim &> /dev/null; then
   if [[ "$OSTYPE" == "darwin"* ]]; then
     brew install neovim
@@ -40,6 +21,25 @@ fi
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
+
+create_link() {
+  local file=$1
+  local executable=$2
+  if [ -e "$HOME/.$file" ]; then
+    echo "Warning: ~/.$file already exists."
+  elif [ -n "$executable" ] && ! command -v $executable &> /dev/null; then
+    echo "Warning: $executable executable not found. Skipping link for $file."
+  else
+    ln -fs "$env_root/dotfiles/$file" "$HOME/.$file"
+  fi
+}
+
+dotfiles=("tmux.conf" "tmux" "aider.conf.yml" "vim" "vimrc" "hyper.js" "alacritty.yml" "config/nvim/init.vim")
+executables=("" "" "aider" "vim" "vim" "hyper" "alacritty" "nvim")
+
+for i in "${!dotfiles[@]}"; do
+  create_link "${dotfiles[$i]}" "${executables[$i]}"
+done
 
 if ! command -v conda &> /dev/null; then
     mkdir -p ~/miniconda3

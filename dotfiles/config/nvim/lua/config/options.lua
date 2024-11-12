2a497e1bc5dfbbcf834d8d1fcf025234e6407976
ZenPuzzle <9865240+ZenPuzzle@users.noreply.github.com>
2024-11-12 02:00:39 +0100

    update nvim config

--- dotfiles/config/nvim/lua/config/options.lua
+++ dotfiles/config/nvim/lua/config/options.lua
@@ -2,4 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.wrap = true
+-- LazyVim auto format
+vim.g.autoformat = true
+
+local opt = vim.opt
+opt.wrap = true
+opt.cursorline = false -- Enable highlighting of the current line
--- dotfiles/config/nvim/lua/plugins/core.lua
+++ dotfiles/config/nvim/lua/plugins/core.lua
@@ -1,16 +1,22 @@
 return {
   -- add shades_of_purple
-  { "Rigellute/shades-of-purple.vim" },
-  { "unrealjo/neovim-purple" },
-  { "catppuccin/nvim", name = "catppuccin" },
+  -- { "Rigellute/shades-of-purple.vim" },
+  -- { "unrealjo/neovim-purple" },
+  -- { "catppuccin/nvim", name = "catppuccin" },
   { "EdenEast/nightfox.nvim" },
-  { "folke/tokyonight.nvim", lazy = false },
+  -- { "folke/tokyonight.nvim", lazy = false },
+  -- disable trouble
+  { "folke/trouble.nvim", enabled = false },
+  -- disable todo-comments
+  { "folke/todo-comments.nvim", enabled = false },
+  -- disable bufferline
+  { "akinsho/bufferline.nvim", enabled = false },
+
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
-      wrap = true,
+      colorscheme = "nordfox",
     },
   },
 }

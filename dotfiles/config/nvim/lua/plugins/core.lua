0f6b28ed935ced226a7e3d4cb56aeaf826e7acec
ZenPuzzle <9865240+ZenPuzzle@users.noreply.github.com>
2024-11-22 00:08:16 +0100

    use snacks terminal instead of toggleterm for better time

--- dotfiles/config/nvim/lua/plugins/core.lua
+++ dotfiles/config/nvim/lua/plugins/core.lua
@@ -12,11 +12,19 @@ return {
   -- disable bufferline
   { "akinsho/bufferline.nvim", enabled = true },
   {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = function()
-      require("toggleterm").setup({ direction = "float", float_opts = { border = "curved" } })
-    end,
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      -- bigfile = { enabled = true },
+      -- notifier = { enabled = true },
+      quickfile = { enabled = true },
+      -- statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
   },
   -- use TAB for completions
   {
